57dba6df80e3ee16286b9d4e21dab002362e06aa
wh079 <mt1337228@gmail.com>
2026-09-18 15:48:56 +0700

    feat: FirstApp

---
+++ FirstApp/app/src/main/java/com/trofimov/firstapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.trofimov.firstapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.trofimov.firstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
